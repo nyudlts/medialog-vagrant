47949932f62d589c2f096c1cc79ab09403dc5003
Donald Mennerich <don.mennerich@nyu.edu>
2016-05-19 08:51:23 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-yum install -y git gpg
+yum install -y git gpg puppet ruby-devel libxml2-devel libxslt-devel postgresql-devel postgresql-contrib zlib-devel gcc-c++
 
 firewall-cmd --zone public --add-port 3000/tcp --permanent
 
 systemctl restart firewalld
-
-yum install -y puppet ruby-devel libxml2-devel libxslt-devel postgresql-devel postgresql-contrib zlib-devel gcc-c++
 
 puppet module install puppetlabs-postgresql
