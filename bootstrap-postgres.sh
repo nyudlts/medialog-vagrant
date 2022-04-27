944bf2fdf9c93359da2d78a19e3b22ffdcf37c84
Donald Mennerich <don.mennerich@nyu.edu>
2022-04-27 10:27:58 -0400

    updating medialog service to use puma

--- bootstrap-postgres.sh
+++ bootstrap-postgres.sh
@@ -6,4 +6,8 @@ yum install -y devtoolset-7 llvm-toolset-7 libpq
 
 yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 
-yum install -y postgresql14-server postgresql14-devel postgresql14-contrib
+yum install -y postgresql14-server
+
+systemctl start medialog
+
+systemctl start nginx
